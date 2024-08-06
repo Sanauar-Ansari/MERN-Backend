e591dc7407cd63c0c9e7ed0e882640a4b9f4769b
Sanauar-Ansari <sanauaransari99@gmail.com>
2024-08-06 18:33:21 +0530

    added cors package

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const app = express();
 const port = 5000;
 const mongoDB = require("./config/mongoose");
 const User = require("./model/User");
+const cors = require("cors");
 //middleware to avoid cross plateform issue
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(bodyParser.json());
 //routes
 app.use("/api", require("./routes/CreateUser"));
